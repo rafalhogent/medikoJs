0970562701767fa74f7803ed67fb809bb449b0f9
Rafi <rafal.kowalski@student.hogent.be>
2024-03-12 22:49:32 +0100

    path to entities in dotenv variable

--- server/db/config/data-source.ts
+++ server/db/config/data-source.ts
@@ -9,7 +9,7 @@ export const dataSourceOptions: DataSourceOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: ['dist/**/*.entity.js'],
+  entities: [process.env.ENTITIES_PATH],
   migrations: ['dist/db/migrations/*.js'],
 };
 
